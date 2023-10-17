c7cfd3a99d92bc6c9a45a381c8e9ed8df827ac79
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2023-10-17 20:57:13 +0300

    Islands can be created and removed from view

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import tkinter as tk
 # import winsound
 import time
@@ -8,18 +9,33 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.colors import ListedColormap
 import threading
 
+colormap = {
+    "ocean_blue": "#0077B6",
+    "grass_green": "#4CAF50"
+}
 
 ikkuna = tk.Tk()
 ikkuna.title("Exercise 8")
 window_width, window_height = 700, 700
 ikkuna.geometry(f"{window_width}x{window_height}")
 
+canvas = tk.Canvas(ikkuna, width=window_width, height=window_height, bg=colormap["ocean_blue"])
+canvas.grid(row=1, column=0, columnspan=6)
 point_button = []
+islands = []
 
-for i in range(5):
-    button_temp = tk.Button(ikkuna, text="Points: "+str(i+1), padx=40)
-    button_temp.grid(row=0, column=i+1)
-    point_button.append(button_temp)
+
+class Island:
+    def __init__(self, canvas):
+        self.width = random.randint(50, 80)
+        self.height = random.randint(50, 80)
+        self.x = random.randint(0, window_width - self.width)
+        self.y = random.randint(0, window_height - self.height)
+        self.color = colormap["grass_green"]
+        self.canvas = canvas
+
+    def draw(self):
+        self.canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill=self.color)
 
 
 def i_suppose_i_have_earned_so_much_points(amount_of_points):
@@ -31,5 +47,44 @@ def i_suppose_i_have_earned_so_much_points(amount_of_points):
         playsound("sounds/point_earned.wav")
 
 
-# colors: sand, pool/digged ground, trench, monkey
-colormap = ListedColormap(["#FFD699", "#FFB266", "#FFEDB3", "#674300", "#0077B6"])
+def check_collision(islands, new_island):
+    for island in islands:
+        if (
+            new_island.x < island.x + island.width and
+            new_island.x + new_island.width > island.x and
+            new_island.y < island.y + island.height and
+            new_island.y + new_island.height > island.y
+        ):
+            return True
+    return False
+
+
+def create_island():
+    if len(islands) < 10:
+        new_island = Island(canvas)
+        while check_collision(islands, new_island):
+            new_island = Island(canvas)  # Generate a new island until it doesn't collide
+
+        new_island.draw()
+        islands.append(new_island)
+
+
+def clear_islands():
+    canvas.delete("all")
+    islands.clear()
+
+
+for i in range(5):
+    button_temp = tk.Button(ikkuna, text="Points: " + str(i + 1), padx=40)
+    button_temp.grid(row=0, column=i + 1)
+    point_button.append(button_temp)
+
+
+button_new_island = tk.Button(ikkuna, text="New Island", command=lambda: create_island())
+button_new_island.grid(row=2, column=1)
+clear_button = tk.Button(ikkuna, text="Clear Islands", command=lambda: clear_islands())
+clear_button.grid(row=2, column=2)
+
+ikkuna.grid_rowconfigure(1, weight=1)
+ikkuna.grid_columnconfigure(0, weight=1)
+ikkuna.mainloop()
